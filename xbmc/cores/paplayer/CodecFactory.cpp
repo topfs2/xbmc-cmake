1a08a8011d17d59e67cb1985edb5fcd087dc88ed
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-16 15:47:34 +0100

    remove nosefart
    now an add-on

--- xbmc/cores/paplayer/CodecFactory.cpp
+++ xbmc/cores/paplayer/CodecFactory.cpp
@@ -21,7 +21,6 @@
 #include "system.h"
 #include "CodecFactory.h"
 #include "OGGcodec.h"
-#include "NSFCodec.h"
 #ifdef HAS_SPC_CODEC
 #include "SPCCodec.h"
 #endif
@@ -80,8 +79,6 @@ ICodec* CodecFactory::CreateCodec(const std::string& strFileType)
     return new DVDPlayerCodec();
   else if (fileType == "wv")
     return new DVDPlayerCodec();
-  else if (fileType == "nsf" || fileType == "nsfstream")
-    return new NSFCodec();
 #ifdef HAS_SPC_CODEC
   else if (fileType == "spc")
     return new SPCCodec();

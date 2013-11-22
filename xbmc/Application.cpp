cc36899ee97d431b2ce6fa503648cdbc941e871b
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-22 19:32:47 +0100

    add support for vfs add-ons

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -126,6 +126,7 @@
 #ifdef HAS_FILESYSTEM_SFTP
 #include "filesystem/SFTPFile.h"
 #endif
+#include "addons/VFSEntry.h"
 #include "PartyModeManager.h"
 #ifdef HAS_VIDEO_PLAYBACK
 #include "cores/VideoRenderers/RenderManager.h"
@@ -3600,6 +3601,7 @@ void CApplication::Stop(int exitCode)
 #ifdef HAS_FILESYSTEM_SFTP
     CSFTPSessionManager::DisconnectAllSessions();
 #endif
+    CVFSEntryManager::Get().DisconnectAll();
 
     CLog::Log(LOGNOTICE, "unload skin");
     UnloadSkin();
@@ -5202,6 +5204,7 @@ void CApplication::ProcessSlow()
 #ifdef HAS_FILESYSTEM_SFTP
   CSFTPSessionManager::ClearOutIdleSessions();
 #endif
+  CVFSEntryManager::Get().ClearOutIdle();
 
   g_mediaManager.ProcessEvents();
 
@@ -5857,4 +5860,5 @@ void CApplication::CloseNetworkShares()
 #ifdef HAS_FILESYSTEM_SFTP
   CSFTPSessionManager::DisconnectAllSessions();
 #endif
+  CVFSEntryManager::Get().DisconnectAll();
 }

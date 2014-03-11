fd41fd3d48b85add5f78d84906242c05e2b2d92b
AlwinEsch <AlwinEsch@github.com>
2014-03-11 23:59:53 +0100

    ADSP - Add audio dsp basic settings dialog

--- xbmc/music/dialogs/GUIDialogMusicOSD.cpp
+++ xbmc/music/dialogs/GUIDialogMusicOSD.cpp
@@ -64,7 +64,14 @@ bool CGUIDialogMusicOSD::OnMessage(CGUIMessage &message)
       return true;
     }
     break;
-  }
+  case GUI_MSG_WINDOW_DEINIT:  // fired when OSD is hidden
+    {
+      // Remove our subdialogs if visible
+      CGUIDialog *pDialog = (CGUIDialog *)g_windowManager.GetWindow(WINDOW_DIALOG_AUDIO_DSP_OSD_SETTINGS);
+      if (pDialog && pDialog->IsDialogRunning()) pDialog->Close(true);
+    }
+    break; 
+ }
   return CGUIDialog::OnMessage(message);
 }
 
---
+++ xbmc/settings/dialogs/GUIDialogAudioSettings.h
@@ -0,0 +1,64 @@
+#pragma once
+/*
+ *      Copyright (C) 2005-2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "cores/AudioEngine/DSPAddons/ActiveAEDSP.h"
+#include "settings/dialogs/GUIDialogSettings.h"
+
+typedef std::vector<int> Features;
+
+class CGUIDialogAudioDSPSettings : public CGUIDialogSettings
+{
+public:
+  CGUIDialogAudioDSPSettings(void);
+  virtual ~CGUIDialogAudioDSPSettings(void);
+
+  virtual bool OnBack(int actionID);
+  virtual void FrameMove();
+
+protected:
+  virtual void CreateSettings();
+  virtual void OnSettingChanged(SettingInfo &setting);
+
+  typedef struct
+  {
+    int              clientId;
+    AE_DSP_MENUHOOK  hook;
+  } MenuHookMember;
+
+  void GoWindowBack(unsigned int prevId);
+  bool SupportsAudioFeature(int feature);
+  void UpdateModeIcons();
+  void AddAddonButtons();
+  void GetAudioDSPMenus(AE_DSP_MENUHOOK_CAT category, std::vector<MenuHookMember> &menus);
+  void OpenAudioDSPMenu(AE_DSP_MENUHOOK_CAT category, ActiveAE::AE_DSP_ADDON client, unsigned int iHookId, unsigned int iLocalizedStringId);
+
+  unsigned int                                m_ActiveStreamId;
+  bool                                        m_AddonMasterModeSetupPresent;             /* If any addon have a own settings dialog for a master mode it is set to true */
+  bool                                        m_streamTypeReset;
+  AE_DSP_STREAMTYPE                           m_streamTypeUsed;
+  float                                       m_volume;
+  AE_DSP_BASETYPE                             m_baseTypeUsed;
+  std::vector<ActiveAE::CActiveAEDSPModePtr>  m_Modes[AE_DSP_ASTREAM_MAX];
+  bool                                        m_ModesPresent;
+  int                                         m_CurrentMenu;
+  std::vector<MenuHookMember>                 m_Menus;
+  Features                                    m_audioCaps;
+};

0a8342ffa8ad988938663b7d027f5ee25e8861b8
AlwinEsch <AlwinEsch@github.com>
2014-03-15 06:08:05 +0100

    Fix segmentation fault with wrong CSampleBuffer type for dsp on CActiveAEBufferPoolResample

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEBuffer.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEBuffer.cpp
@@ -198,7 +198,7 @@ bool CActiveAEBufferPoolResample::Create(unsigned int totaltime, bool remap, boo
       if (m_processor->GetChannelLayout().Count() > 2)                  /* Disable upmix for CActiveAEResample if DSP layout > 2.0, becomes perfomed by DSP */
         upmix = false;
 
-      m_dspBuffer = new CActiveAEBufferPool(m_inputFormat);             /* Get dsp processing buffer class */
+      m_dspBuffer = new CActiveAEBufferPool(m_format);                  /* Get dsp processing buffer class, based on output format */
       m_dspBuffer->Create(totaltime);
     }
   }

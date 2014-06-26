963bc6f9d5f3c0a606c0c1d9618893e99442108f
Arne Morten Kvarving <cptspiff@gmail.com>
2014-06-26 11:19:43 +0200

    changed: get rid of CStdString in GUIDialogAudioSubtitleSettings

--- xbmc/video/dialogs/GUIDialogAudioSubtitleSettings.cpp
+++ xbmc/video/dialogs/GUIDialogAudioSubtitleSettings.cpp
@@ -197,7 +197,7 @@ void CGUIDialogAudioSubtitleSettings::OnSettingAction(const CSetting *setting)
   const std::string &settingId = setting->GetId();
   if (settingId == SETTING_SUBTITLE_BROWSER)
   {
-    CStdString strPath;
+    std::string strPath;
     if (URIUtils::IsInRAR(g_application.CurrentFileItem().GetPath()) || URIUtils::IsInZIP(g_application.CurrentFileItem().GetPath()))
       strPath = CURL(g_application.CurrentFileItem().GetPath()).GetHostName();
     else
@@ -409,7 +409,7 @@ void CGUIDialogAudioSubtitleSettings::AddAudioStreams(CSettingGroup *group, cons
   // left, right or both
   if (g_application.m_pPlayer->GetAudioStreamCount() == 1)
   {
-    CStdString strAudioInfo;
+    std::string strAudioInfo;
     g_application.m_pPlayer->GetAudioInfo(strAudioInfo);
 
     /* TODO:STRING_CLEANUP */
@@ -465,7 +465,7 @@ void CGUIDialogAudioSubtitleSettings::AudioStreamsOptionFiller(const CSetting *s
   for (int i = 0; i < audioStreamCount; ++i)
   {
     std::string strItem;
-    CStdString strLanguage;
+    std::string strLanguage;
 
     SPlayerAudioStreamInfo info;
     g_application.m_pPlayer->GetAudioStreamInfo(i, info);
@@ -499,8 +499,8 @@ void CGUIDialogAudioSubtitleSettings::SubtitleStreamsOptionFiller(const CSetting
     SPlayerSubtitleStreamInfo info;
     g_application.m_pPlayer->GetSubtitleStreamInfo(i, info);
 
-    CStdString strItem;
-    CStdString strLanguage;
+    std::string strItem;
+    std::string strLanguage;
 
     if (!g_LangCodeExpander.Lookup(strLanguage, info.language))
       strLanguage = g_localizeStrings.Get(13205); // Unknown
